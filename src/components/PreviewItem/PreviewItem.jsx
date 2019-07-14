07b0eddf4b392f08b7b4d5f8b63e8eff23f9c3c0
alexander-berezhnoy <berrezhnoy@gmail.com>
2019-07-14 15:55:54 +0300

    Updated shop data and implemented function for grouping cart items

--- src/components/PreviewItem/PreviewItem.jsx
+++ src/components/PreviewItem/PreviewItem.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cartActions";
 import CustomButton from "../CustomButton/CustomButton";
 import "./PreviewItem.scss";
 
-const PreviewItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const PreviewItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        {" "}
+        Add to cart{" "}
+      </CustomButton>
     </div>
-    <CustomButton inverted> Add to cart </CustomButton>
-  </div>
-);
+  );
+};
 
-export default PreviewItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(PreviewItem);
