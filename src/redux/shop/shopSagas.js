7a2092682dc31bb6303e1ff55125d140d3a1d8e4
alexander-berezhnoy <berrezhnoy@gmail.com>
2019-07-25 23:04:25 +0300

    Switched from Redux Thunk to Redux Saga and implemented saga for fetching shop data

---
+++ src/redux/shop/shopSagas.js
@@ -0,0 +1,34 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from "./shopActions";
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase";
+
+import ShopActionTypes from "./shopActionTypes";
+
+export function* fetchCollectionAsync() {
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionAsync
+  );
+}
